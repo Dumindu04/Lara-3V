ddf4864b776c879828b8b434e881254068e464c8
Dumindu04 <166401628+Dumindu04@users.noreply.github.com>
2026-03-27 00:03:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,10 +6,10 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94779062397' : process.env.OWNER_NUMBER, //ADD YOUR NUMBER
-    SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID, //ADD YOUR SSION ID
-    GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? '' : process.env.GITHUB_USER_NAME, //ADD YOUR GITHUB USERNAME
-    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN //ADD YOUR GITHUB AUTH TOKEN WITHOUT gph_ ,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
+    OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94765744087' : process.env.OWNER_NUMBER, //ADD YOUR NUMBER
+    SESSION_ID: process.env.SESSION_ID === undefined ? '𝕃𝔸ℝ𝔸-𝕄𝔻=CNYhCS4Y#taggBvArI3EaLxGob-R04yL3xTVfw-jvqVV9AP4GfGE' : process.env.SESSION_ID, //ADD YOUR SSION ID
+    GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'Dumindu04' : process.env.GITHUB_USER_NAME, //ADD YOUR GITHUB USERNAME
+    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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' : process.env.GITHUB_AUTH_TOKEN //ADD YOUR GITHUB AUTH TOKEN WITHOUT gph_ ,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
 };
 
 /*
